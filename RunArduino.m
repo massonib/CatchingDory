9f2538ad9c8e7b9fcc464f633155e6b24d9e5cde
massonib <massonib@onid.oregonstate.edu>
2016-05-17 17:31:04 -0700

    Added module to run the arduino

---
+++ RunArduino.m
@@ -0,0 +1,37 @@
+% clear
+% clc
+%  
+% answer=5; % this is where we'll store the user's answer
+arduino=serial('COM4','BaudRate',9600); % create serial communication object on port COM4
+fopen(arduino); % initiate arduino communication
+
+try
+pause(5);
+% value = '5';
+%fprintf(arduino, '%s', value); % send answer variable content to arduino
+fwrite(arduino, '9');
+fscanf(arduino)
+pause(4);
+fwrite(arduino, '1');
+pause(2);
+fwrite(arduino, '5'); 
+pause(2);
+fwrite(arduino, '6');
+pause(10);
+fwrite(arduino, '9');
+pause(4);
+fwrite(arduino, '2');
+pause(2);
+fwrite(arduino, '5'); 
+pause(2);
+fwrite(arduino, '6');
+pause(10);
+fwrite(arduino, '9');
+pause(4);
+
+fclose(arduino); 
+
+catch me
+    fclose(arduino); %Make absolutely sure this is closed
+end
+%delete(arduino);
